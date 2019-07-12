a154d99916172820584cb507e4a98a0bdba7c04f
akulsrivastava <akulsr0@gmail.com>
2019-07-12 22:59:02 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-import clsx from 'clsx';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
 
 export default class App extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      username: ''
+      username: '',
+      data: [],
+      profilePic: '',
+      followers: '',
+      following: '',
+      bio: ''
     }
   }
 
@@ -18,31 +21,105 @@ export default class App extends React.Component {
 
   }
 
+  getDataFromApi = () => {
+    return fetch("https://www.instagram.com/"+this.state.username+"/?__a=1")
+      .then(response => response.json())
+      .then((jsonData) => {
+        this.state.data = []
+        this.setState({data: this.state.data.concat(jsonData)})
+        var objArr = this.state.data[0];
+        var picUrl = objArr.graphql.user.profile_pic_url_hd;
+        var followerCount = objArr.graphql.user.edge_followed_by.count;
+        var followingCount = objArr.graphql.user.edge_follow.count;
+        var bio = objArr.graphql.user.biography;
+        this.setState({profilePic: picUrl, followers: followerCount, following: followingCount, bio: bio})
+        console.log(this.state)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  };
+
   render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div style={{marginTop: 20}}>
-            <a style={{fontSize: 60, fontWeight: 'bold', fontFamily: 'Montserrat'}}>InstaView</a>
-          </div>
-          <div style={{width: '80%',}}>
-            <TextField
-              id="outlined-email-input"
-              style={{width: '100%',}}
-              label="Username"
-              type="text"
-              name="username"
-              margin="normal"
-              variant="outlined"
-              onChange={value=>{this.setState({username: value.target.value})}}
-            />
-            <Button variant="contained" color="primary" style={{background: '#3C40C6'}} onClick={()=>{console.log(this.state.username)}}>
-              get results
-            </Button>
-          </div>
-        </header>
-      </div>
-    );
+
+    if(this.state.data.length===0){
+      return(
+        <div className="App">
+          <header className="App-header">
+            <div style={{marginTop: 20}}>
+              <a style={{fontSize: 60, fontWeight: 'bold', fontFamily: 'Montserrat'}}>InstaView</a>
+            </div>
+            <div style={{width: '80%',}}>
+              <TextField
+                id="outlined-email-input"
+                style={{width: '100%',}}
+                label="Username"
+                type="text"
+                name="username"
+                margin="normal"
+                variant="outlined"
+                onChange={value=>{this.setState({username: value.target.value})}}
+              />
+              <Button variant="contained" color="primary" style={{background: '#3C40C6'}} onClick={()=>{
+                this.getDataFromApi()
+              }}>
+                Search
+              </Button>
+            </div>
+          </header>
+        </div>
+      );
+    }
+    else{
+      return (
+        <div className="App">
+          <header className="App-header">
+            <div style={{marginTop: 20}}>
+              <a style={{fontSize: 60, fontWeight: 'bold', fontFamily: 'Montserrat'}}>InstaView</a>
+            </div>
+            <div style={{width: '80%',}}>
+              <TextField
+                id="outlined-email-input"
+                style={{width: '100%',}}
+                label="Username"
+                type="text"
+                name="username"
+                margin="normal"
+                variant="outlined"
+                onChange={value=>{this.setState({username: value.target.value})}}
+              />
+              <Button variant="contained" color="primary" style={{background: '#3C40C6'}} onClick={()=>{
+                this.getDataFromApi()
+              }}>
+                Search
+              </Button>
+            </div>
+            
+            <div style={{width: '100%', marginTop: 30, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+              <div style={{width: 250, height: 250}}>
+                <img src={this.state.profilePic} style={{width:'100%', height:'100%', borderRadius: '5%'}} />
+              </div>
+              <div style={{width: '100%', marginTop: 20}}>
+                <Button variant="contained" color="primary" style={{background: '#3C40C6'}} onClick={()=>{
+                  window.open(this.state.profilePic, "_blank");
+                }}>
+                  Download Image
+                </Button>
+              </div>
+              <div style={{width: '100%', marginTop: 10}}>
+                <a style={{color:'#000', fontFamily: 'Roboto Condensed'}}>Followers: {this.state.followers}</a>
+                <a style={{color:'#000', fontFamily: 'Roboto Condensed', marginLeft: 10}}>Following: {this.state.following}</a>
+              </div>
+              <div style={{width: '100%', marginTop: 10}}>
+                <a style={{color:'#000', fontFamily: 'Roboto Condensed'}}>{this.state.bio}</a>
+              </div>
+            </div>
+
+          </header>
+        </div>
+      );
+    }
+
   }
 
 }
