a2866daa760fb9793976cabf2a6c85c219801586
akulsrivastava <akulsr0@gmail.com>
2019-11-19 00:40:13 +0530

    added likescheck

--- src/App.js
+++ src/App.js
@@ -1,144 +1,19 @@
-import React from 'react';
-import './App.css';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-export default class App extends React.Component {
+import DPDown from './DPDownView'
+import LikeCountView from './LikeCountView'
 
-  constructor(props){
-    super(props);
-    this.state = {
-      username: '',
-      data: [],
-      profilePic: '',
-      followers: '',
-      following: '',
-      bio: '',
-      postsCount: '',
-      isPrivate: '',
-      postsNode: '',
-    }
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={DPDown} />
+          <Route path="/dp-download" component={DPDown} />
+          <Route path="/check-likes" component={LikeCountView} />
+        </Switch>
+      </BrowserRouter>
+    )
   }
-
-  componentDidMount(){
-
-  }
-
-  getDataFromApi = () => {
-    return fetch("https://www.instagram.com/"+this.state.username+"/?__a=1")
-      .then(response => response.json())
-      .then((jsonData) => {
-        this.state.data = []
-        this.setState({data: this.state.data.concat(jsonData)})
-        var objArr = this.state.data[0];
-        var picUrl = objArr.graphql.user.profile_pic_url_hd;
-        var followerCount = objArr.graphql.user.edge_followed_by.count;
-        var followingCount = objArr.graphql.user.edge_follow.count;
-        var bio = objArr.graphql.user.biography;
-        var postsCount = objArr.graphql.user.edge_owner_to_timeline_media.count;
-        var isPrivate = objArr.graphql.user.is_private;
-        var postsNode = objArr.graphql.user.edge_owner_to_timeline_media.edges;
-        
-
-        this.setState({profilePic: picUrl, followers: followerCount, following: followingCount, bio: bio, postsCount: postsCount, isPrivate: isPrivate, postsNode: postsNode})
-
-
-        console.log(this.state)
-
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  };
-
-
-  render(){
-
-    if(this.state.data.length===0){
-      return(
-        <div className="App">
-          <header className="App-header">
-            <div style={{marginTop: 20}}>
-              <a style={{fontSize: 60, fontWeight: 'bold', fontFamily: 'Montserrat'}}>InstaView</a>
-            </div>
-            <div style={{width: '80%',}}>
-              <TextField
-                id="outlined-email-input"
-                style={{width: '100%',}}
-                label="Username"
-                type="text"
-                name="username"
-                margin="normal"
-                variant="outlined"
-                onChange={value=>{this.setState({username: value.target.value})}}
-              />
-              <Button variant="contained" color="primary" style={{background: '#000'}} onClick={()=>{
-                this.getDataFromApi()
-              }}>
-                Search
-              </Button>
-            </div>
-          </header>
-        </div>
-      );
-    }
-    else{
-      return (
-        <div className="App">
-          <header className="App-header">
-            <div style={{marginTop: 20}}>
-              <a style={{fontSize: 60, fontWeight: 'bold', fontFamily: 'Montserrat'}}>InstaView</a>
-            </div>
-            <div style={{width: '80%',}}>
-              <TextField
-                id="outlined-email-input"
-                style={{width: '100%',}}
-                label="Username"
-                type="text"
-                name="username"
-                margin="normal"
-                variant="outlined"
-                onChange={value=>{this.setState({username: value.target.value})}}
-              />
-              <Button variant="contained" color="primary" style={{background: '#000'}} onClick={()=>{
-                this.getDataFromApi()
-              }}>
-                Search
-              </Button>
-            </div>
-            
-            <div style={{width: '100%', marginTop: 30, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-              <div style={{width: 250, height: 250}}>
-                <img src={this.state.profilePic} style={{width:'100%', height:'100%', borderRadius: '5%'}} />
-              </div>
-              <div style={{width: '100%', marginTop: 20}}>
-                <Button variant="contained" color="primary" style={{background: '#000'}} onClick={()=>{
-                  window.open(this.state.profilePic, "_blank");
-                }}>
-                  Download Image
-                </Button>
-              </div>
-              <div style={{width: '100%', marginTop: 15}}>
-                <a style={{fontSize: 18, color:'#000', fontFamily: 'Roboto Condensed'}}>Posts: {this.state.postsCount}</a>
-              </div>
-              <div style={{width: '100%', marginTop: 2}}>
-                <a style={{fontSize: 18, color:'#000', fontFamily: 'Roboto Condensed'}}>Followers: {this.state.followers}</a>
-                <a style={{fontSize: 18, color:'#000', fontFamily: 'Roboto Condensed', marginLeft: 10}}>Following: {this.state.following}</a>
-              </div>
-              <div style={{width: '100%', marginTop: 2}}>
-                <a style={{fontSize: 18, color:'#000', fontFamily: 'Roboto Condensed'}}>{this.state.bio}</a>
-              </div>
-
-              
-              <div style={{height: 60}}></div>
-
-            </div>
-
-          </header>
-        </div>
-      );
-    }
-
-  }
-
 }
---
+++ src/LikeCountView.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react'
+import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { NavLink } from "react-router-dom";
+
+export default class LikeCountView extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            likes: null,
+            postURL: '',
+            data: [],
+        }
+    }
+
+    getLikesCount = () => {
+
+        var {postURL} = this.state
+        var url = postURL.split('?')[0]
+        
+        return fetch(url+"?__a=1")
+        .then(response => response.json())
+        .then((jsonData) => {
+            this.state.data = []
+            this.setState({data: this.state.data.concat(jsonData.graphql.shortcode_media)})
+            var likes = this.state.data[0].edge_media_preview_like.count;
+            this.setState({likes})
+            console.log(this.state.likes)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+
+    render() {
+        if(this.state.likes===null) {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                    <div style={{marginTop: 20}}>
+                    <a style={{fontSize: 60, fontWeight: 'bold', fontFamily: 'Montserrat'}}>InstaView</a>
+                    </div>
+    
+                    <div style={{width: '100%'}}>
+                    <nav class="navbar navbar-expand-mg navbar-dark" style={{backgroundColor: '#333'}}>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                        <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <NavLink style={{ textDecoration: "none", color: "#fff", fontWeight: 'bold', padding: 10}} to="/">DP-Download</NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink style={{ textDecoration: "none", color: "#fff", fontWeight: "bold", padding: 10}} to="/check-likes">Check Like Count</NavLink>
+                        </li>
+                        </ul>
+                        </div>
+                    </nav>
+                </div> 
+
+                <div style={{marginTop: 10}} >
+                        <small>Note: This will work for only public profiles.</small>
+                    </div>
+    
+                <div style={{width: '80%',}}>
+                  <TextField
+                    id="outlined-email-input"
+                    style={{width: '100%',}}
+                    label="Post URL"
+                    type="text"
+                    name="username"
+                    margin="normal"
+                    variant="outlined"
+                    onChange={value=>{this.setState({postURL: value.target.value})}}
+                  />
+                  <Button variant="contained" color="primary" style={{background: '#000'}} onClick={()=>{
+                      this.getLikesCount()
+                    }}>
+                    Check
+                  </Button>
+                </div>
+              </header>
+            </div>
+            )
+        } else {
+            return (
+            <div className="App">
+                <header className="App-header">
+                    <div style={{marginTop: 20}}>
+                    <a style={{fontSize: 60, fontWeight: 'bold', fontFamily: 'Montserrat'}}>InstaView</a>
+                    </div>
+    
+                    <div style={{width: '100%'}}>
+                    <nav class="navbar navbar-expand-mg navbar-dark" style={{backgroundColor: '#333'}}>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                        <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <NavLink style={{ textDecoration: "none", color: "#fff", fontWeight: 'bold', padding: 10}} to="/">DP-Download</NavLink>
+                        </li>
+                        <li class="nav-item">
+                            <NavLink style={{ textDecoration: "none", color: "#fff", fontWeight: "bold", padding: 10}} to="/check-likes">Check Like Count</NavLink>
+                        </li>
+                        </ul>
+                        </div>
+                    </nav>
+                </div> 
+    
+                    <div style={{marginTop: 10}} >
+                        <small>Note: This will work for only public profiles.</small>
+                    </div>
+
+                    <div style={{width: '80%',}}>
+                  <TextField
+                    id="outlined-email-input"
+                    style={{width: '100%',}}
+                    label="Post URL"
+                    type="text"
+                    name="username"
+                    margin="normal"
+                    variant="outlined"
+                    onChange={value=>{this.setState({postURL: value.target.value})}}
+                  />
+                  <Button variant="contained" color="primary" style={{background: '#000'}} onClick={()=>{
+                      this.getLikesCount()
+                    }}>
+                    Check
+                  </Button>
+                </div>
+                
+                    <div style={{width: '100%', marginTop: 40}}>
+                        <h3>Likes: {this.state.likes}</h3>
+                    </div>
+                
+                </header>
+            </div>
+            )
+        }
+    }
+}
